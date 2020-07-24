e453360e184a101b0fe0820dbe36cbbe20a337d8
huadeyi <hdy1101496521@163.com>
2020-07-24 18:53:37 +0800

    配置文件对日志输出的影响

--- src/main/java/com/hua/design_demo/DesignDemoApplication.java
+++ src/main/java/com/hua/design_demo/DesignDemoApplication.java
@@ -1,13 +1,19 @@
 package com.hua.design_demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class DesignDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DesignDemoApplication.class, args);
+        log.debug("debug");
+        log.info("info");
+        log.warn("warn");
+        log.error("error");
     }
 
 }
